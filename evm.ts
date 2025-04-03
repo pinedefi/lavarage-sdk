af630c5112a1a71a5fd1e361690ccc14c1f67931
trishateh <trishateh@gmail.com>
2025-04-03 13:40:15 +0800

    feat: add other getter functions, tidy up interfaces

--- evm.ts
+++ evm.ts
@@ -7,60 +7,25 @@ import {
   ZeroAddress,
 } from "ethers";
 import { borrowerOperationsAbi } from "./abi/borrowerOperations";
-
-export interface BorrowerOperationsContract {
-  buy(
-    buyingCode: string,
-    tokenCollateral: string,
-    borrowAmount: BigNumberish,
-    tokenHolder: string,
-    inchRouter: string,
-    integratorFeeAddress: string
-  ): Promise<ContractTransaction>;
-  sell(
-    loanId: BigNumberish,
-    sellingCode: string,
-    tokenHolder: string,
-    inchRouter: string,
-    integratorFeeAddress: string
-  ): Promise<ContractTransaction>;
-}
-
-export interface BuyEvent {
-  trader: string;
-  tokenCollateral: string;
-  loanId: bigint;
-  openingPositionSize: bigint;
-  collateralAmount: bigint;
-  initialMargin: bigint;
-}
-
-export interface SellEvent {
-  trader: string;
-  tokenCollateral: string;
-  loanId: bigint;
-  closingPositionSize: bigint;
-  profit: bigint;
-}
-
-export interface LiquidationEvent {
-  trader: string;
-  tokenCollateral: string;
-  loanId: bigint;
-  closingPositionSize: bigint;
-  liquidatorRepaidAmount: bigint;
-}
+import { tokenHolderAbi } from "./abi/tokenHolderAbi";
+import {
+  BuyEvent,
+  SellEvent,
+  LiquidationEvent,
+  Loan,
+  Collateral,
+} from "./interfaces/evm";
 
 /**
  * Opens a trading position on EVM chain
  * @param signer - Ethers signer
- * @param contractAddress - BorrowerOperations contract address
+ * @param borrowerOpsContractAddress - BorrowerOperations contract address
  * @param params - Trading parameters
  * @returns Transaction object
  */
 export const openPositionEvm = async (
   signer: Signer,
-  contractAddress: string,
+  borrowerOpsContractAddress: string,
   {
     buyingCode,
     tokenCollateral,
@@ -79,7 +44,11 @@ export const openPositionEvm = async (
     buyerContribution: BigNumberish;
   }
 ): Promise<ContractTransaction> => {
-  const contract = new Contract(contractAddress, borrowerOperationsAbi, signer);
+  const contract = new Contract(
+    borrowerOpsContractAddress,
+    borrowerOperationsAbi,
+    signer
+  );
 
   return contract.buy(
     buyingCode,
@@ -95,13 +64,13 @@ export const openPositionEvm = async (
 /**
  * Closes a trading position on EVM chain
  * @param signer - Ethers signer
- * @param contractAddress - BorrowerOperations contract address
+ * @param borrowerOpsContractAddress - BorrowerOperations contract address
  * @param params - Trading parameters
  * @returns Transaction object
  */
 export const closePositionEvm = async (
   signer: Signer,
-  contractAddress: string,
+  borrowerOpsContractAddress: string,
   {
     loanId,
     sellingCode,
@@ -116,7 +85,11 @@ export const closePositionEvm = async (
     integratorFeeAddress?: string;
   }
 ): Promise<ContractTransaction> => {
-  const contract = new Contract(contractAddress, borrowerOperationsAbi, signer);
+  const contract = new Contract(
+    borrowerOpsContractAddress,
+    borrowerOperationsAbi,
+    signer
+  );
 
   return contract.sell(
     loanId,
@@ -130,17 +103,17 @@ export const closePositionEvm = async (
 /**
  * Get all positions from events
  * @param provider - Ethers provider
- * @param contractAddress - BorrowerOperations contract address
+ * @param borrowerOpsContractAddress - BorrowerOperations contract address
  * @param fromBlock - Block to start searching from
  * @returns Array of Buy events representing positions
  */
 export async function getPositionsEvm(
   provider: Provider,
-  contractAddress: string,
+  borrowerOpsContractAddress: string,
   fromBlock: number = 0
 ): Promise<BuyEvent[]> {
   const contract = new Contract(
-    contractAddress,
+    borrowerOpsContractAddress,
     borrowerOperationsAbi,
     provider
   );
@@ -171,17 +144,17 @@ export async function getPositionsEvm(
 /**
  * Get closed positions from events
  * @param provider - Ethers provider
- * @param contractAddress - BorrowerOperations contract address
+ * @param borrowerOpsContractAddress - BorrowerOperations contract address
  * @param fromBlock - Block to start searching from
  * @returns Array of Sell events representing closed positions
  */
 export async function getClosedPositionsEvm(
   provider: Provider,
-  contractAddress: string,
+  borrowerOpsContractAddress: string,
   fromBlock: number = 0
 ): Promise<SellEvent[]> {
   const contract = new Contract(
-    contractAddress,
+    borrowerOpsContractAddress,
     borrowerOperationsAbi,
     provider
   );
@@ -205,17 +178,17 @@ export async function getClosedPositionsEvm(
 /**
  * Get liquidated positions from events
  * @param provider - Ethers provider
- * @param contractAddress - BorrowerOperations contract address
+ * @param borrowerOpsContractAddress - BorrowerOperations contract address
  * @param fromBlock - Block to start searching from
  * @returns Array of Liquidation events representing liquidated positions
  */
 export async function getLiquidatedPositionsEvm(
   provider: Provider,
-  contractAddress: string,
+  borrowerOpsContractAddress: string,
   fromBlock: number = 0
 ): Promise<LiquidationEvent[]> {
   const contract = new Contract(
-    contractAddress,
+    borrowerOpsContractAddress,
     borrowerOperationsAbi,
     provider
   );
@@ -239,4 +212,107 @@ export async function getLiquidatedPositionsEvm(
       liquidatorRepaidAmount,
     };
   });
+}
+
+/**
+ * Get a loan by its ID from TokenHolder contract
+ * @param provider - Ethers provider
+ * @param tokenHolderContractAddress - Address of the TokenHolder contract
+ * @param loanId - ID of the loan to retrieve
+ * @returns Loan object
+ */
+export async function getLoanEvm(
+  provider: Provider,
+  tokenHolderContractAddress: string,
+  loanId: BigNumberish
+): Promise<Loan> {
+  const contract = new Contract(
+    tokenHolderContractAddress,
+    tokenHolderAbi,
+    provider
+  );
+  return contract.loans(loanId);
+}
+
+/**
+ * Get all active loans for a user
+ * @param provider - Ethers provider
+ * @param tokenHolderContractAddress - Address of the TokenHolder contract
+ * @param userAddress - Address of the user to get loans for
+ * @returns Array of loans belonging to the user
+ */
+export async function getUserLoansEvm(
+  provider: Provider,
+  tokenHolderContractAddress: string,
+  userAddress: string
+): Promise<Loan[]> {
+  const contract = new Contract(
+    tokenHolderContractAddress,
+    tokenHolderAbi,
+    provider
+  );
+  const nextLoanId = await contract.nextLoanId();
+  const userLoans: Loan[] = [];
+
+  // Iterate through all loans to find the ones belonging to the user
+  for (let i = 0; i < nextLoanId; i++) {
+    const loan = await contract.loans(i);
+    if (loan.borrower.toLowerCase() === userAddress.toLowerCase()) {
+      userLoans.push(loan);
+    }
+  }
+
+  return userLoans;
+}
+
+/**
+ * Get collateral information for a specific token
+ * @param provider - Ethers provider
+ * @param tokenHolderContractAddress - Address of the TokenHolder contract
+ * @param collateralAddress - Address of the collateral token
+ * @returns Collateral object
+ */
+export async function getCollateralInfoEvm(
+  provider: Provider,
+  tokenHolderContractAddress: string,
+  collateralAddress: string
+): Promise<Collateral> {
+  const contract = new Contract(
+    tokenHolderContractAddress,
+    tokenHolderAbi,
+    provider
+  );
+  return contract.collateralMapping(collateralAddress);
+}
+
+/**
+ * Get all available collateral information
+ * @param provider - Ethers provider
+ * @param tokenHolderContractAddress - Address of the TokenHolder contract
+ * @param collateralAddresses - Array of collateral token addresses
+ * @returns Array of active collaterals
+ */
+export async function getOffersEvm(
+  provider: Provider,
+  tokenHolderContractAddress: string,
+  collateralAddresses: string[]
+): Promise<Array<{ address: string; collateral: Collateral }>> {
+  const contract = new Contract(
+    tokenHolderContractAddress,
+    tokenHolderAbi,
+    provider
+  );
+  const activeCollaterals = [];
+
+  for (const address of collateralAddresses) {
+    const collateral = await contract.collateralMapping(address);
+    if (collateral.active) {
+      activeCollaterals.push({
+        address,
+        collateral,
+      });
+    }
+  }
+
+  return activeCollaterals;
 }
